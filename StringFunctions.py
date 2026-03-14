b1b2b19a404e87bd9a3bc804aedaedd930a59fc5
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-14 23:17:31 +0530

    Create StringFunctions.py

---
+++ StringFunctions.py
@@ -0,0 +1,10 @@
+s = "hello world"
+
+print("upper:", s.upper())
+print("lower:", s.lower())
+print("title:", s.title())
+print("capitalize:", s.capitalize())
+print("count:", s.count("l"))
+print("find:", s.find("world"))
+print("replace:", s.replace("world", "python"))
+print("split:", s.split())
