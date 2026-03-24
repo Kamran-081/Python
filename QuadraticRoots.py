0cca53b9e4025965b10c1c8fb67e3cf4eadc92c7
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-24 21:57:53 +0530

    Create QuadraticRoots.py

---
+++ QuadraticRoots.py
@@ -0,0 +1,18 @@
+import math
+
+a, b, c = map(float, input().split())
+
+d = b*b - 4*a*c
+
+if d > 0:
+    r1 = (-b + math.sqrt(d)) / (2*a)
+    r2 = (-b - math.sqrt(d)) / (2*a)
+    print(r1, r2)
+elif d == 0:
+    r = -b / (2*a)
+    print(r, r)
+else:
+    real = -b / (2*a)
+    imag = math.sqrt(-d) / (2*a)
+    print(real, "+", imag, "i")
+    print(real, "-", imag, "i")
